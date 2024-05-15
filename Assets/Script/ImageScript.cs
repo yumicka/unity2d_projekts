b94a9a865c09cec6fbdc5fe7dac710db7c3ef477
Alina Bokareva <nikagirl2124@gmail.com>
2024-05-15 09:33:53 +0300

    Pirmas lekcijas beigas

--- Assets/Script/ImageScript.cs
+++ Assets/Script/ImageScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageScript : MonoBehaviour
 {
@@ -8,10 +9,16 @@ public class ImageScript : MonoBehaviour
     public GameObject teddy;
     public GameObject lady;
     public GameObject car;
+    public GameObject leftToggle;
+    public GameObject rightToggle;
+    public Sprite[] spriteArray;
+    public GameObject imageHolder;
 
     public void showBean(bool value)
     {
         bean.SetActive(value);
+        leftToggle.GetComponent<Toggle>().interactable = value;
+        rightToggle.GetComponent<Toggle>().interactable = value;
     }
 
     public void showTeddy(bool value)
@@ -29,5 +36,26 @@ public class ImageScript : MonoBehaviour
         car.SetActive(value);
     }
 
+    public void toLeft()
+    {
+        bean.transform.localScale = new Vector2(1, 1);
+    }
+
+    public void toRight()
+    {
+        bean.transform.localScale = new Vector2(-1, 1);
+    }
+
+    public void changeImage(int index)
+    {
+        if (index == 0)
+            imageHolder.GetComponent<Image>().sprite = spriteArray[0];
+
+        else if (index == 1)
+            imageHolder.GetComponent<Image>().sprite = spriteArray[1];
+
+        else if (index == 12)
+            imageHolder.GetComponent<Image>().sprite = spriteArray[2];
+    }
 
 }
